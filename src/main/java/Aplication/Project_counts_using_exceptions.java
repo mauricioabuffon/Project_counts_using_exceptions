d9fd58de77b2f38ce21902b97ce1b644a1f77913
Mauricio Antonio Buffon <mauricio.a.buffon@gmail.com>
2023-08-09 10:28:07 -0300

    including Account Class

---
+++ src/main/java/Aplication/Project_counts_using_exceptions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Aplication;
+
+/**
+ *
+ * @author socorro
+ */
+public class Project_counts_using_exceptions {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+//        System.out.println("Hello World!");
+    }
+}
